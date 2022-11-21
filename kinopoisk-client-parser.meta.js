63676f719a813f0be0be18b52519a048954e3ec3
crashmax-dev <crashmax.dev@gmail.com>
2022-11-21 15:33:05 +0000

    Deploying to gh-pages from @ crashmax-dev/kinopoisk-client-parser@6ba7ad776c4407de29781198485e89f2e15375bd 🚀

--- kinopoisk-client-parser.meta.js
+++ kinopoisk-client-parser.meta.js
@@ -3,8 +3,7 @@
 // @version     0.0.1
 // @license     MIT
 // @homepage    https://crashmax-dev.github.io/kinopoisk-client-parser/
-// @match       https://www.kinopoisk.ru/lists/categories/movies/*
-// @match       https://www.kinopoisk.ru/lists/movies/*
+// @match       https://www.kinopoisk.ru/**
 // @updateURL   https://crashmax-dev.github.io/kinopoisk-client-parser/kinopoisk-client-parser.meta.js
 // @downloadURL https://crashmax-dev.github.io/kinopoisk-client-parser/kinopoisk-client-parser.user.js
 // ==/UserScript==
