f8bac8739fbd755e94d72bbe330f7894592739b0
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-11-22 01:02:20 +0800

    upd

--- index.js
+++ index.js
@@ -9,9 +9,5 @@ const movies = JSON.parse(
 
 for (const movie of movies) {
   const transactionID = Math.random().toString(16).slice(2)
-  await fetch("https://gql.twitch.tv/gql", {
-  "headers": {},
-  "body": `[{\"operationName\":\"RedeemCustomReward\",\"variables\":{\"input\":{\"channelID\":\"156632065\",\"cost\":1,\"prompt\":null,\"rewardID\":\"cdaf337e-7eaf-4adb-a4ef-08e476d4d649\",\"textInput\":\"${movie}\",\"title\":\"test\",\"transactionID\":\"${transactionID}\"}},\"extensions\":{\"persistedQuery\":{\"version\":1,\"sha256Hash\":\"d56249a7adb4978898ea3412e196688d4ac3cea1c0c2dfd65561d229ea5dcc42\"}}}]`,
-  "method": "POST"
-});
+  // fetch(...)
 }
